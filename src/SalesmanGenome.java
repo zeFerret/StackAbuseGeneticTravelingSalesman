c4233a9940d6c98b4c9122ecfe37664ddcf24b7e
Justas SESKEVICIUS <justinas@inventi.lt>
2020-09-11 00:06:09 +0300

    add comments to genome

--- src/SalesmanGenome.java
+++ src/SalesmanGenome.java
@@ -1,12 +1,21 @@
 import java.util.*;
 
 public class SalesmanGenome implements Comparable<SalesmanGenome> {
+    // The list with the cities in order in which they should be visited
+    // This sequence represents the solution to the problem
     List<Integer> genome;
+
+    // Travel prices are handy to be able to calculate fitness
     int[][] travelPrices;
+
+    // While the starting city doesn't change the solution of the problem,
+    // it's handy to just pick one so you could rely on it being the same
+    // across genomes
     int startingCity;
     int numberOfCities;
     int fitness;
 
+    // Generates a random salesman
     public SalesmanGenome(int numberOfCities, int[][] travelPrices, int startingCity) {
         this.travelPrices = travelPrices;
         this.startingCity = startingCity;
@@ -15,6 +24,7 @@ public class SalesmanGenome implements Comparable<SalesmanGenome> {
         fitness = this.calculateFitness();
     }
 
+    // Generates a salesman with a user-defined genome
     public SalesmanGenome(List<Integer> permutationOfCities, int numberOfCities, int[][] travelPrices, int startingCity) {
         genome = permutationOfCities;
         this.travelPrices = travelPrices;
@@ -26,14 +36,22 @@ public class SalesmanGenome implements Comparable<SalesmanGenome> {
     public int calculateFitness() {
         int fitness = 0;
         int currentCity = startingCity;
+
+        // Calculating path cost
         for (int gene : genome) {
             fitness += travelPrices[currentCity][gene];
             currentCity = gene;
         }
+
+        // We have to add going back to the starting city to complete the circle
+        // the genome is missing the starting city, and indexing starts at 0, which is why we subtract 2
         fitness += travelPrices[genome.get(numberOfCities - 2)][startingCity];
         return fitness;
     }
 
+    // Generates a random genome
+    // Genomes are permutations of the list of cities, except the starting city
+    // so we add them all to a list and shuffle
     private List<Integer> randomSalesman() {
         List<Integer> result = new ArrayList<Integer>();
         for (int i = 0; i < numberOfCities; i++) {
